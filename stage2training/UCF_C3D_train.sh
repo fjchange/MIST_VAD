216010358c0a10fdd6655d784152dfec89b2d96c
kiwi_fung <fjchange@hotmail.com>
2021-09-13 21:45:47 +0800

    Update UCF_C3D_train.sh

--- stage2training/UCF_C3D_train.sh
+++ stage2training/UCF_C3D_train.sh
@@ -1,1 +1,1 @@
-python UCF_C3D_train.py --gpus 0,1,2,3 --gpu0sz 0.8 --lr 1e-4 --batch_size 10 --clip_num 3 --pretrained_backbone --warmup_epochs 5 --epochs 301
+python UCF_C3D_train.py --gpus 0,1,2,3 --gpu0sz 0.8 --lr 1e-4 --batch_size 32 --clip_num 3 --warmup_epochs 5 --epochs 301 --expand_k 8 --freeze_epochs 60 --freeze_backbone
