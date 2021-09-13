9b9bb4a80bddd8fd091f8d09d66eb903e0357d0c
kiwi_fung <fjchange@hotmail.com>
2021-09-13 21:09:33 +0800

    Update constant.py

--- configs/constant.py
+++ configs/constant.py
@@ -10,36 +10,35 @@ The paths below should be modified to adapt your project
 ############# 1. Model Paths #######################
 
 ## The pretrained model paths
-_C.I3D_MODEL_PATH='pretrained/model_rgb.pth'
-_C.C3D_MODEL_PATH='pretrained/C3D_Sport1M/pth'
+_C.I3D_MODEL_PATH='/data2/jiachang/model_rgb.pth'
+_C.C3D_MODEL_PATH='/data2/jiachang/C3D_Sport1M/pth'
 
 ## Trained Ckpts
-_C.UCF_C3D_MODEL_PATH='ckpts/UCF_C3D_AUC_0.8143.pth'
-_C.UCF_I3D_MODEL_PATH='ckpts/UCF_I3D_AUC_0.8230.pth'
-_C.SHT_C3D_MODEL_PATH='ckpts/SHT_C3D_AUC_0.9313.pth'
-_C.STH_I3D_MODEL_PATH='ckpts/SHT_I3D_AUC_0.9483.pth'
+_C.UCF_C3D_MODEL_PATH='../ckpts/UCF_C3D_AUC_0.8143.pth'
+_C.UCF_I3D_MODEL_PATH='../ckpts/UCF_I3D_AUC_0.8230.pth'
+_C.SHT_C3D_MODEL_PATH='../ckpts/SHT_C3D_AUC_0.9313.pth'
+_C.STH_I3D_MODEL_PATH='../ckpts/SHT_I3D_AUC_0.9483.pth'
 
 ## Vis Paths
-_C.VIS_DIR='outputs/'
-_C.TEST_SPATIAL_ANNOTATION_PATH='data/Test_Spatial_Annotation.npy'
+_C.TEST_SPATIAL_ANNOTATION_PATH='../data/Test_Spatial_Annotation.npy'
 ############ 2. UCF Data ###########################
-_C.TRAIN_H5_PATH='data/UCFCrime-Frames.h5'
-_C.TEST_H5_PATH='data/UCFCrime-Frames-test.h5'
-_C.TESTING_TXT_PATH='data/Temporal_Anomaly_Annotation_New.txt'
+_C.TRAIN_H5_PATH='/data2/jiachang/UCFCrime-Frames.h5'
+_C.TEST_H5_PATH='/data2/jiachang/UCFCrime-Frames-test.h5'
+_C.TESTING_TXT_PATH='../data/Temporal_Anomaly_Annotation_New.txt'
 
-_C.PSEUDO_LABEL_PATH_I3D='data/UCF_I3D_PLs.npy'
-_C.PSEUDO_LABEL_PATH_C3D='data/UCF_C3D_PLs.npy'
+_C.PSEUDO_LABEL_PATH_I3D='../data/UCF_I3D_PLs.npy'
+_C.PSEUDO_LABEL_PATH_C3D='../data/UCF_C3D_PLs.npy'
 
 ############# 3. SHT Data ##########################
-_C.SHT_TRAIN_H5_PATH='data/SHT_Frames.h5'
-_C.SHT_TEST_H5_PATH='data/SHT_Frames.h5'
+_C.SHT_TRAIN_H5_PATH='/data2/jiachang/SHT_Frames.h5'
+_C.SHT_TEST_H5_PATH='/data2/jiachang/SHT_Frames.h5'
 
-_C.SHT_TEST_MASK_DIR='data/test_frame_mask/'
-_C.SHT_TRAIN_TXT_PATH='data/SH_Train_new.txt'
-_C.SHT_TEST_TXT_PATH='data/SH_Test_NEW.txt'
+_C.SHT_TEST_MASK_DIR='../data/test_frame_mask/'
+_C.SHT_TRAIN_TXT_PATH='../data/SH_Train_new.txt'
+_C.SHT_TEST_TXT_PATH='../data/SH_Test_NEW.txt'
 
-_C.PSEUDO_LABEL_PATH_SHT_I3D='data/SHT_I3D_PLs.npy'
-_C.PSEUDO_LABEL_PATH_SHT_C3D='data/SHT_C3D_PLs.npy'
+_C.PSEUDO_LABEL_PATH_SHT_I3D='../data/SHT_I3D_PLs.npy'
+_C.PSEUDO_LABEL_PATH_SHT_C3D='../data/SHT_C3D_PLs.npy'
 
 ############# 4. Dataset Related ###################
 _C.DATASET=CfgNode()
@@ -55,7 +54,17 @@ _C.DATASET.C3D_STD=[1,1,1]
 
 ############# 5.training setting ##################
 _C.SEED=0
-_C.LOG_DIR='logs/'
-_C.SUMMARY_DIR='summarys/'
-_C.MODEL_DIR='train_ckpts/'
+_C.LOG_DIR='../logs/'
+_C.SUMMARY_DIR='../summarys/'
+_C.MODEL_DIR='../train_ckpts/'
+_C.VIS_DIR='../outputs/'
 
+import os
+def mkdir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+mkdir(_C.LOG_DIR)
+mkdir(_C.SUMMARY_DIR)
+mkdir(_C.MODEL_DIR)
+mkdir(_C.VIS_DIR)
