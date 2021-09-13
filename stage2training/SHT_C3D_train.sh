4b7a0ebfee4da323c212596325ae9ee76e8af096
kiwi_fung <fjchange@hotmail.com>
2021-09-13 22:01:47 +0800

    Update SHT_C3D_train.sh

--- stage2training/SHT_C3D_train.sh
+++ stage2training/SHT_C3D_train.sh
@@ -1,1 +1,1 @@
-python SHT_C3D_train.py --gpus 0,1,2,3 --gpu0sz 0.8 --lr 1e-4 --batch_size 10 --clip_num 3 --pretrained_backbone --warmup_epochs 5 --epochs 301
+python SHT_C3D_train.py --gpus 0,1,2,3 --gpu0sz 0.8 --lr 1e-4 --batch_size 10 --clip_num 3 --warmup_epochs 5 --epochs 301 freeze_epochs 30 --freeze_backbone
